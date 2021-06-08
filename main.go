3b5a8c7adcc0c1bd1ac1e287df7b062112102468
arrizalrahmat <arrizalrahmat@gmail.com>
2021-06-08 11:12:45 +0700

    done

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"go-assignment2/controllers"
+	"go-assignment2/models"
+
+	"github.com/gin-contrib/cors" // cors
+	"github.com/gin-gonic/gin"    // gin
+	// "gorm.io/driver/postgres"
+	// "gorm.io/gorm"
+)
+
+func main() {
+	models.Init() // init db
+
+	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"http://localhost:8080"},
+		AllowMethods: []string{"GET", "POST", "PUT", "DELETE"},
+		// AllowHeaders:     []string{"Origin"},
+		// ExposeHeaders:    []string{"Content-Length"},
+		// AllowCredentials: true,
+	}))
+
+	// db := models.SetupModels()
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{"data": "hello"})
+	})
+
+	orders := router.Group("/orders")
+	{
+		orders.GET("/", controllers.GetOrders)
+		orders.POST("/", controllers.CreateOrder)
+		orders.PUT("/:orderId", controllers.UpdateOrder)
+		orders.DELETE("/:orderId", controllers.DeleteOrder)
+	}
+
+	items := router.Group("/items")
+	{
+		items.GET("/", controllers.GetItems)
+	}
+
+	router.Run()
+}
